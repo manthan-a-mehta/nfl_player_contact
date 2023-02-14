5389dafbc044a50263a38a9efdb453e5422fe29f
Manthan Mehta <63070765+manthan-a-mehta@users.noreply.github.com>
2023-02-13 19:42:59 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import torch
 BATCH_SIZE = 8 # increase / decrease according to GPU memeory
-RESIZE_TO = 416 # resize the image for training and transforms
-NUM_EPOCHS = 1 # number of epochs to train for
+RESIZE_TO = 224 # resize the image for training and transforms
+NUM_EPOCHS = 5 # number of epochs to train for
 NUM_WORKERS = 4
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 # training images and XML files directory
-TRAIN_DIR = '/home/manthan/capstone/data/images/'
+TRAIN_DIR = '/kaggle/working/images/'
 # validation images and XML files directory
-VALID_DIR = '/home/manthan/capstone/data/valid'
-TRAINING_FILE_PATH="/home/manthan/capstone/ds5500_capstone_project_nfl/train.csv"
-VALID_FILE_PATH="/home/manthan/capstone/ds5500_capstone_project_nfl/valid.csv"
+VALID_DIR = '/kaggle/working/images/'
+TRAINING_FILE_PATH="train_mini.csv"
+VALID_FILE_PATH="valid.csv"
 
 # classes: 0 index is reserved for background
 CLASSES = [
